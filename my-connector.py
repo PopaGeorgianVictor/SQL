fb1b939746aa383629f313c0ae3dda2555bf7622
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-13 12:16:02 +0200

    sql

--- my-connector.py
+++ my-connector.py
@@ -3,6 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='popa1993'
-    port='3306'
+    password='popa1993',
+    port='3306',
+    database='petclinic'
 )
