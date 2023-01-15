04e791a94c036d6863a096a26389d958bc657991
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-15 12:13:58 +0200

    sql-query

--- northwind.py
+++ northwind.py
@@ -7,3 +7,14 @@ mydb = mysql.connector.connect(
     port='3306',
     database='northwind'
 )
+
+'''RETURN THE PRODUCT NAME, QUANTITY PER UNIT AND UNIT PRICE FOR ALL PRODUCTS ORDER BY PRODUCT NAME DESCENDING'''
+
+mycursor = mydb.cursor()
+mycursor.execute('SELECT ProductName, QuantityPerUnit, UnitPrice FROM Products '
+                 'ORDER BY ProductName DESC ')
+
+query1  = mycursor.fetchall()
+
+for q1 in query1:
+    print(q1)
