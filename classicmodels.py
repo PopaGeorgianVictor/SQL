d864f94ddbfa852aaeac40a186addb5867049df8
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-06-28 20:25:26 +0300

    classicmodels

--- classicmodels.py
+++ classicmodels.py
@@ -10,3 +10,22 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+
+'''Check table presence in database schema'''
+
+mycursor.execute('show tables')
+
+query1 = mycursor.fetchall()
+for q1 in query1:
+    print(q1)
+
+
+    
+'''Check number of columns in a table'''
+
+mycursor.execute('SELECT count(*) AS NumbersOfColumns FROM information_schema.columns WHERE table_name = "customers" ')
+
+query1 = mycursor.fetchall()
+for q1 in query1:
+    print(q1)
