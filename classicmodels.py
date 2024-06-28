183c1eb1e1d08ed66de7483c84a1ef9812db975b
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-06-28 17:42:54 +0300

    classicmodels

---
+++ classicmodels.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='popa1993',
+    port='3306',
+    database='classicmodels'
+)
+
+mycursor = mydb.cursor()
