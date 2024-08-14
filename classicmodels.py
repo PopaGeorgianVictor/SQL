084ec7aa7cb3739d5cb23f952b0eb290643a39ca
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-08-14 17:07:53 +0300

    trigger

--- classicmodels.py
+++ classicmodels.py
@@ -208,3 +208,18 @@ for q in query:
 # CREATE TABLE WorkCenterStats( 
 #    totalCapacity INT NOT NULL
 # );
+
+# delimiter //
+
+# CREATE TRIGGER before_workcenters_insert BEFORE INSERT ON WorkCenters FOR EACH ROW 
+# BEGIN
+# 	DECLARE rowcount INT;
+# 	SELECT COUNT(*) INTO rowcount FROM WorkCenterStats;
+# 	IF rowcount > 0 THEN
+#     UPDATE WorkCenterStats SET totalCapacity = totalCapacity + new.capacity;
+#     ELSE
+# 	INSERT INTO WorkCenterStats (totalCapacity) VALUES (new.capacity);
+# END IF;
+# END //
+
+# delimiter ;
